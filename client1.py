fe964575060c6e76d8d0810c8738e204ae1a0b38
arainrabee19 <bsef14m523@pucit.edu.pk>
2016-06-26 16:43:56 +0500

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,42 +1,34 @@
+# Socket-Assignment
+
 import socket  # Import socket module
 from thread import *
-import sys
 
+s = socket.socket()
+HOST = ''
+port = 5178  # Reserve a port for your service.
 
-
-host = socket.gethostname()
-buffer=2048
-port = 5005  # Reserve a port for your service.
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host, port))
-
-s.listen(10)
+s.connect((HOST, port))
 print s.recv(1024)
 
-c=[]
-
-c.append(s)
-
-print "Chat server started on port " + str(port)
-
-
+data=raw_input("Your Name: ")
+s.send(data)
+second=raw_input("Chat with: ")
+s.send(second)
 def receive():
     while True:
         data=s.recv(1024)
         print data
 
-start_new_thread(receive, ())
+start_new_thread(receive, (s,))
 while 1:
-    name=raw_input('your name: ')
-    s.send(name)
-    name=raw_input('second user name: ')
-    s.send(name)
-
-while True:
-    data=raw_input('Me bola hun: ')
-    s.send(data)
+    rep=raw_input()
+    if rep=='q':
+        s.send(rep)
+        break
+    else:
+        s.send(rep)
 
 
-s.close()
+
+
+s.close
